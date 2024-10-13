379c686650aad5e6968350c887f37ed66fed1216
orangeri <135411258+orangeri@users.noreply.github.com>
2024-10-14 00:12:22 +0700

    Update auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -1,12 +1,21 @@
-import { Hono } from "jsr:@hono/hono";
-import { bearerAuth } from "jsr:@hono/hono/bearer-auth";
-import { BlankEnv, BlankSchema } from "jsr:@hono/hono/types";
+import { Hono } from "jsr:@hono/hono";  
+import { bearerAuth } from "jsr:@hono/hono/bearer-auth";  
+import { BlankEnv, BlankSchema } from "jsr:@hono/hono/types";  
 
-function auth(app: Hono<BlankEnv, BlankSchema, "/">) {
-  const token = Deno.env.get("TOKEN");
-  if (token) {
-    app.use("/v1/*", bearerAuth({ token }));
-  }
-}
+function auth(app: Hono<BlankEnv, BlankSchema, "/">) {  
+  const tokens: string[] = [];  
+  
+   
+  for (const [key, value] of Object.entries(Deno.env.toObject())) {  
+      
+    if (key.startsWith("TOKEN")) {  
+      tokens.push(value);  
+    }  
+  }  
+
+  if (tokens.length > 0) {  
+    app.use("/v1/*", bearerAuth({ token: tokens }));  
+  }  
+}  
 
 export { auth };
