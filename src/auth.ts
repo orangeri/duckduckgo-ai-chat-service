efd5fd1e07788a433bac1c9693921d8f43cfaaa5
orangeri <135411258+orangeri@users.noreply.github.com>
2024-10-14 00:09:20 +0700

    Update auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -1,21 +1,12 @@
-import { Hono } from "jsr:@hono/hono";  
-import { bearerAuth } from "jsr:@hono/hono/bearer-auth";  
-import { BlankEnv, BlankSchema } from "jsr:@hono/hono/types";  
+import { Hono } from "jsr:@hono/hono";
+import { bearerAuth } from "jsr:@hono/hono/bearer-auth";
+import { BlankEnv, BlankSchema } from "jsr:@hono/hono/types";
 
-function auth(app: Hono<BlankEnv, BlankSchema, "/">) {  
-  const tokens: string[] = [];  
-  
-  // Mengambil semua environment variables  
-  for (const [key, value] of Object.entries(Deno.env.toObject())) {  
-    // Memeriksa jika key dimulai dengan "TOKEN"  
-    if (key.startsWith("TOKEN")) {  
-      tokens.push(value);  
-    }  
-  }  
-
-  if (tokens.length > 0) {  
-    app.use("/v1/*", bearerAuth({ token: tokens }));  
-  }  
-}  
+function auth(app: Hono<BlankEnv, BlankSchema, "/">) {
+  const token = Deno.env.get("TOKEN");
+  if (token) {
+    app.use("/v1/*", bearerAuth({ token }));
+  }
+}
 
 export { auth };
